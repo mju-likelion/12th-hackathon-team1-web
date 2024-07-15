74476e6c559f2392811f546dffc1762088038ac0
jogpfls <joerin4177@gmail.com>
2024-07-15 23:41:04 +0900

    setting: 초기설정 수정

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -3,32 +3,47 @@ import './font.css';
 
 export const Theme = {
     fonts: {
-        default: css`
+        title40: css`
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 600;
+            font-size: 40px;
+        `,
+        title32: css`
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 500;
+            font-size: 32px;
+        `,
+        bigButtonText: css`
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 500;
+            font-size: 24px;
+        `,
+        title20: css`
             font-family: "Noto Sans KR", sans-serif;
             font-weight: 400;
             font-size: 20px;
         `,
-        bigText: css`
-            font-family: "Noto Sans KR", sans-serif;
-            font-weight: 500;
-            font-size: 64px;
-        `,
-        bigButtonText: css`
-            font-family: "Noto Sans KR", sans-serif;
-            font-weight: 700;
-            font-size: 48px;
-        `,
-        menuText: css`
+        default18: css`
             font-family: "Noto Sans KR", sans-serif;
             font-weight: 400;
             font-size: 18px;
         `,
-        smallText200: css`
+        munuText: css`
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 700;
+            font-size: 16px;
+        `,
+        default16: css`
             font-family: "Noto Sans KR", sans-serif;
             font-weight: 400;
             font-size: 16px;
         `,
-        smallText100: css`
+        helpText14: css`
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 400;
+            font-size: 14px;
+        `,
+        helpText12: css`
             font-family: "Noto Sans KR", sans-serif;
             font-weight: 400;
             font-size: 12px;
@@ -36,10 +51,17 @@ export const Theme = {
     },
 
     colors: {
-        green: '#D7EFC8',
-        green100: '#B7E899',
-        green200: '#A3C087',
-        green300: '#4FD80E',
-        red: '#FF5858',
+        backgroundColor: "#F7F7F7",
+        helperText: "#9B9B9B",
+        greenButton: "#B7E899",
+        black: '#000000',
+        white: '#FFFFFF',
+        error: "#FF5858",
+        check: "#4FD80E",
+        green100: "#E6FADC",
+        green200: "#D7EFC8",
+        percentRed: "#FF9797",
+        dateGray: "#B6B6B6",
+        ingredient: "#FCFDDA",
     },
 };
