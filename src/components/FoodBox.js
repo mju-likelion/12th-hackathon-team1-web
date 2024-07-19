7d608fffc155f48b7fb2e3a18cfb42318946551c
jogpfls <joerin4177@gmail.com>
2024-07-19 20:40:53 +0900

    feat: 메인페이지 UI 구현

--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const FoodBox = () => {
+const FoodBox = (props) => {
     return (
         <Box>
             <TextWrapper>
-                <IngredientName>사과</IngredientName>
-                <IngredientDate>유통기한: 2024.08.07</IngredientDate>
+                <IngredientName isDate={props.id === "date"}>사과</IngredientName>
+                <IngredientDate isDate={props.id === "date"}>유통기한: 2024.08.07</IngredientDate>
             </TextWrapper>
         </Box>
     );
@@ -19,6 +19,7 @@ const Box = styled.div`
     border-radius: 23px;
     display: flex;
     justify-content: center;
+    flex-shrink: 0;
 
     @media screen and (max-width: 1200px) {
         width: 10vw;
@@ -32,10 +33,13 @@ const TextWrapper = styled.div`
     flex-direction: column;
     justify-content: center;
     gap: 20%;
+
 `;
 
 const IngredientName = styled.p`
     ${({theme})=>theme.fonts.default18}
+    color: ${({theme, isDate})=> (isDate ? theme.colors.dateGray : 'initial')};
+
     @media screen and (max-width: 1200px){
         font-size: 1.3vw;
     }
@@ -43,6 +47,7 @@ const IngredientName = styled.p`
 
 const IngredientDate = styled.p`
     ${({theme})=>theme.fonts.default12}
+    color: ${({theme, isDate})=> (isDate ? theme.colors.dateGray : 'initial')};
 
     @media screen and (max-width:1200px){
         font-size: 0.8vw;
--- src/components/RefrigeratorBox.js
+++ src/components/RefrigeratorBox.js
@@ -1,20 +1,131 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import RefrigeratorSection from "./RefrigeratorSection"
+import FoodBox from './FoodBox';
+import arrow from '../assets/images/next.svg';
 
-const RefrigeratorBox = () => {
+const RefrigeratorBox = (props) => {
+    const [ButtonText, setButtonText] = useState('편집');
+
+    const toggleButton = () => {
+        setButtonText(preText => (preText === '편집' ? '저장' : '편집'));
+    }
+
     return (
         <Wrapper>
-            <TopLine />
-            <LinBox>
-                <RefrigeratorSection />
-                <Line />
-                <RefrigeratorSection />
-                <Line />
-                <RefrigeratorSection />
-                <Line />
-                <RefrigeratorSection />
-            </LinBox>
+            <div>
+                <TopLine>
+                    <Button onClick={toggleButton}>{ButtonText}</Button>
+                </TopLine>
+                <BottomBox>
+                    <SectionBox>
+                        <WrapperWrapper>
+                            <Section>
+                                <AllTextWrapper>
+                                    <TextBox>
+                                        <Text>유통기한 만료</Text>
+                                    </TextBox>
+                                    <LineBox>
+                                        <VerticalLine />
+                                    </LineBox>
+                                    <FoodBoxWrapper>
+                                        <FoodBox id = "date"/>
+                                        <FoodBox id="date" />
+                                        <FoodBox id="date" />
+                                        <FoodBox id="date" />
+                                        <FoodBox id="date"/>
+                                        <FoodBox id="date"/>
+                                        <FoodBox id="date"/>
+                                    </FoodBoxWrapper>
+                                </AllTextWrapper>
+                            </Section>
+                            <ArrowBox>
+                                <Arrow src={arrow} alt="화살표"/>
+                            </ArrowBox>
+                        </WrapperWrapper>
+                    </SectionBox>
+                    <Line />
+                    <SectionBox>
+                        <WrapperWrapper>
+                            <Section>
+                                <AllTextWrapper>
+                                    <TextBox>
+                                        <Text>냉동실</Text>
+                                    </TextBox>
+                                    <LineBox>
+                                        <VerticalLine />
+                                    </LineBox>
+                                    <FoodBoxWrapper>
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                    </FoodBoxWrapper>
+                                </AllTextWrapper>
+                            </Section>
+                            <ArrowBox>
+                                <Arrow src={arrow} alt="화살표"/>
+                            </ArrowBox>
+                        </WrapperWrapper>
+                    </SectionBox>
+                    <Line />
+                    <SectionBox>
+                        <WrapperWrapper>
+                            <Section>
+                                <AllTextWrapper>
+                                    <TextBox>
+                                        <Text>냉장실</Text>
+                                    </TextBox>
+                                    <LineBox>
+                                        <VerticalLine />
+                                    </LineBox>
+                                    <FoodBoxWrapper>
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                    </FoodBoxWrapper>
+                                </AllTextWrapper>
+                            </Section>
+                            <ArrowBox>
+                                <Arrow src={arrow} alt="화살표"/>
+                            </ArrowBox>
+                        </WrapperWrapper>
+                    </SectionBox>
+                    <Line />
+                    <SectionBox>
+                        <WrapperWrapper>
+                            <Section>
+                                <AllTextWrapper>
+                                    <TextBox>
+                                        <Text>상온</Text>
+                                    </TextBox>
+                                    <LineBox>
+                                        <VerticalLine />
+                                    </LineBox>
+                                    <FoodBoxWrapper>
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                        <FoodBox />
+                                    </FoodBoxWrapper>
+                                </AllTextWrapper>
+                            </Section>
+                            <ArrowBox>
+                                <Arrow src={arrow} alt="화살표"/>
+                            </ArrowBox>
+                        </WrapperWrapper>
+                    </SectionBox>
+                </BottomBox>
+            </div> 
         </Wrapper>
     );
 };
@@ -22,10 +133,10 @@ const RefrigeratorBox = () => {
 const Wrapper = styled.div`
     background-color: ${({theme})=> theme.colors.green200};
     width: 1150px;
-    height: 42vw;
+    height: 772px;
     border-radius: 10px;
     display: flex;
-    flex-direction: column;
+    justify-content: center;
     align-items: center;
 
     @media screen and (max-width: 1200px){
@@ -36,27 +147,160 @@ const Wrapper = styled.div`
 
 `;
 
+
 const TopLine = styled.div`
-    width: 92%;
-    height: 8%;
+    width: 1058px;
+    height: 61px;
     border-radius: 10px;
     background-color: ${({theme})=>theme.colors.white};
-    margin-top: 5%;
-    margin-bottom: 2%;
+    margin-bottom: 30px;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+
+    @media screen and (max-width: 1200px){
+        width: 73.5vw;
+        height: 4.24vw;
+        border-radius: 0.52vw;
+        margin-bottom: 2vw;
+    }
 `;
 
-const LinBox = styled.div`
-    width: 92%;
-    height: 75%;
+const Button = styled.button`
+    background-color: ${({theme})=> theme.colors.green200};
+    width: 140px;
+    height: 36px;
+    border-radius: 10px;
+    margin-right: 10px;
+
+    &:hover {
+        background-color: ${({theme})=>theme.colors.greenButton};
+    }
+
+    @media screen and (max-width:1200px){
+        width: 10vw;
+        height: 2.57vw;
+        border-radius: 0.7vw;
+    }
+`;
+
+
+const BottomBox = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 3%;
+    gap: 20px;
+
+    @media screen and (max-width: 1200px){
+        gap: 1.3vw;
+    }
+`;
+
+const SectionBox = styled.div`
+    width: 1058px;
+    height: 118px;
+    display: flex;
+    flex-direction: column;
+
+    @media screen and (max-width: 1200px){
+        width: 73.5vw;
+        height: 8.5vw;
+    }
+`;
+
+const WrapperWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+`;
+
+const Section = styled.div`
+    background-color: ${({theme})=> theme.colors.green100};
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+`;
+
+const AllTextWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+`;
+
+const TextBox = styled.div`
+    width: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Text = styled.p`
+    @media screen and (max-width: 1200px){
+        font-size: 1.3vw;
+    }
+`;
+
+const LineBox = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const VerticalLine = styled.div`
+    background-color: ${({theme})=>theme.colors.white};
+    width: 3px;
+    height: 90px;
+    
+    @media screen and (max-width: 1200px){
+        width: 0.4vw;
+        height: 4.5vw;
+    }
+`;
+
+const FoodBoxWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 50px;
+    margin-left: 50px;
+    overflow-x: auto;
+    white-space: nowrap;
+
+    &::-webkit-scrollbar{
+        display: none;
+    }
+
+    @media screen and (max-width: 1200px){
+        margin-left: 2.5vw;
+        gap: 2.5vw;
+    }
 `;
 
 const Line = styled.div`
-    width: 100%;
-    height: 4%;
+    width: 1058px;
+    height: 3px;
     background-color: ${({theme})=>theme.colors.white};
+
+    @media screen and (max-width: 1200px){
+        width: 73.5vw;
+    }
+`;
+
+const ArrowBox = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const Arrow = styled.img`
+    width: 25px;
+    height: 25px;
+    margin-left: 10px;
+    cursor: pointer;
+
+    @media screen and (max-width: 1200px){
+        width: 1.5vw;
+        height: 1.5vw;
+        margin: 0.9vw;
+    }
 `;
 
 export default RefrigeratorBox;
