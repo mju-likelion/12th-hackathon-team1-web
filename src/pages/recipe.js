7fdfaefb05891216fd161c645432cdc1cf42a949
hoiee04 <chaehl1004@gmail.com>
2024-07-20 15:55:46 +0900

    feat: 파일명 수정 전 기본 로직 구현

---
+++ src/pages/recipe.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Recipe = () => {
+  return <Container />;
+};
+
+const Container = styled.div`
+  display: flex;
+`;
+
+export default Recipe;
