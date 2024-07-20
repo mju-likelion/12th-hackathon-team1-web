89e2436fb0c2973e513d8854c1a3b162f9243735
hoiee04 <chaehl1004@gmail.com>
2024-07-20 16:01:57 +0900

    feat: 타이틀 코드

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -2,11 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 const Recipe = () => {
-  return <Wrapper />;
+  return (
+    <Wrapper>
+      <BoxTitle>인기 레시피</BoxTitle>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
   display: flex;
+`;
+
+const BoxTitle = styled.p`
+  font-size: ${({ theme }) => theme.fonts.default18};
 `;
 
 export default Recipe;
