e4985ce34dddc2ce78378cc68d0ff542af1027db
go_d_0 <rhejrdud527@gmail.com>
2024-07-22 16:06:48 +0900

    feat:Login

--- src/components/BigButton.js
+++ src/components/BigButton.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const BigButton = () => {
-    return (
-        <Btn>
-        </Btn>
-    );
+const BigButton = ({ disabled, onClick, children }) => {
+  return (
+    <Btn disabled={disabled} onClick={onClick}>
+      {children}
+    </Btn>
+  );
 };
 
 const Btn = styled.button`
-    width: 400px;
-    height: 60px;
-    border-radius: 10px;
-    background-color: ${({ theme }) => theme.colors.greenButton};
-    color: ${({ theme }) => theme.colors.white};
-    ${({ theme }) => theme.fonts.bigButtonText}
+  width: 400px;
+  height: 60px;
+  border-radius: 10px;
+  background-color: ${({ disabled, theme }) =>
+    disabled ? theme.colors.dateGray : theme.colors.greenButton};
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.fonts.bigButtonText};
 `;
 
 export default BigButton;
--- src/components/InputFilled.js
+++ src/components/InputFilled.js
@@ -1,20 +1,66 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const InputFilled = () => {
-    return (
-        <InputBox>
-        </InputBox>
-    );
+const InputFilled = ({
+  placeholder,
+  type,
+  value,
+  onChange,
+  hint,
+  error,
+  success,
+}) => {
+  return (
+    <InputContainer>
+      <Filled
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onChange={onChange}
+        isValid={!error && value !== ""}
+        isSuccess={success}
+      />
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      {!error && !value && hint && <Hint>{hint}</Hint>}
+      {success && <SuccessMessage>{success}</SuccessMessage>}
+    </InputContainer>
+  );
 };
 
-const InputBox = styled.input`
-    width: 400px;
-    height: 60px;
-    border-radius: 10px;
-    border: none;
-    border-bottom: 1px solid ${({ theme }) => theme.colors.helperText};
-    background-color: ${({theme})=>theme.colors.white};
+const InputContainer = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+
+const Filled = styled.input`
+  width: 400px;
+  min-height: 60px;
+  border: 1px solid
+    ${({ isValid, theme }) => (isValid ? theme.colors.green : "white")};
+  border-radius: 10px;
+  ${({ theme }) => theme.fonts.default};
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  &:focus {
+    outline: none;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  color: ${({ theme }) => theme.colors.error};
+  margin-top: 5px;
+  ${({ theme }) => theme.fonts.helpText12};
+`;
+
+const Hint = styled.div`
+  color: ${({ theme }) => theme.colors.helperText};
+  margin-top: 5px;
+  ${({ theme }) => theme.fonts.helpText12};
+`;
+
+const SuccessMessage = styled.div`
+  color: ${({ theme }) => theme.colors.check};
+  margin-top: 5px;
+  ${({ theme }) => theme.fonts.helpText12};
 `;
 
 export default InputFilled;
