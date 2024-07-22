0698fbda2023a40ef6fe6b1af2bad07364dff1ad
hoiee04 <chaehl1004@gmail.com>
2024-07-23 03:22:01 +0900

    feat: 콘솔 오류 해결

--- src/components/RecipePage/SidebarItem.js
+++ src/components/RecipePage/SidebarItem.js
@@ -1,12 +1,12 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Arrow from "../../assets/images/next.svg";
 
 function SidebarItem({ menu, isSelected }) {
   return (
-    <ItemContainer isSelected={isSelected}>
+    <ItemContainer $isSelected={isSelected}>
       {menu.name}
-      {isSelected && <Icon src={Arrow} alt="화살표 아이콘" />}
+      {isSelected && <img src={Arrow} alt="화살표 아이콘" />}
     </ItemContainer>
   );
 }
@@ -17,13 +17,17 @@ const ItemContainer = styled.p`
   align-items: center;
   font-size: ${({ theme }) => theme.fonts.default16};
   margin: 10px 0;
-  font-weight: ${(props) => (props.isSelected ? 600 : 400)};
   cursor: pointer;
+
+  ${({ $isSelected }) =>
+    $isSelected &&
+    css`
+      font-weight: 600;
+    `}
+
   &:hover {
     font-weight: 600;
   }
 `;
-
-const Icon = styled.img``;
 
 export default SidebarItem;
