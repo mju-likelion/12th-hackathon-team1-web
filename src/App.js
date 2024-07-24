2c6ecd3fce481950e33973713e5ba67200022884
jogpfls <joerin4177@gmail.com>
2024-07-24 12:47:55 +0900

    fix: app 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Layout from "./components/Layout";
-import Main from "./pages/Main";
 
 function App() {
   return (
     <div>
       <Layout />
-      <Main />
     </div>
   );
 }
