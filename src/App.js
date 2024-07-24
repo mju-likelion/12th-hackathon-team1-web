54fc9b2840ff12a39a076ea64a16608fb9fd8da6
jogpfls <joerin4177@gmail.com>
2024-07-24 15:10:56 +0900

    feat: app route 연결

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout";
+import Main from './pages/Main';
+import Login from './pages/Login';
+import Join from './pages/Join';
+import Refrigerator from "./pages/Fridge";
+import Recipe from './pages/Recipe';
 
 function App() {
   return (
-    <div>
+    <>
       <Layout />
-    </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/main" />}/>
+        <Route path="/main" element={<Main />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/signin" element={<Join />} />
+        <Route path="/fridge" element={<Refrigerator />} />
+        <Route path="/recipe" element={<Recipe />} />
+      </Routes>
+    </>
   );
 }
 
